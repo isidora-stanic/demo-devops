aabe167194cc4eedde1b4d7ffff1c2f745379bee
isidora-stanic <isidora.stanic@outlook.com>
2023-07-13 21:21:33 +0200

    just demo1

--- demo3mongodb/src/test/java/com/example/demo3mongodb/Demo3mongodbApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo3mongodb;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Demo3mongodbApplicationTests {
-
-}
